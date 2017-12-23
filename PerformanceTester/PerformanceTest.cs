a641c34b8ab566b4ca268d28743a03a31881d32e
Jason <dibiancoj@gmail.com>
2017-12-22 21:12:07 -0500

    add seperate class

---
+++ PerformanceTester/PerformanceTest.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerformanceTester
+{
+
+    [MemoryDiagnoser]
+    public class PerformanceTest
+    {
+
+        // *** to run from command line prompt "dotnet run -c Release" ***
+
+        [GlobalSetup]
+        public void Init()
+        {
+        }
+
+        private Guid MyProperties { get; set; } = Guid.NewGuid();
+
+        [Benchmark]
+        public Task<Guid> Task_Test()
+        {
+            return Task.FromResult(MyProperties);
+        }
+
+        [Benchmark]
+        public ValueTask<Guid> ValueTask_Test()
+        {
+            return new ValueTask<Guid>(MyProperties);
+        }
+
+    }
+
+}
