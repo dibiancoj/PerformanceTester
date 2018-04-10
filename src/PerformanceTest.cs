f483a135ebe97dad25d3c51f4a7727d5ceafd5dc
Jason <dibiancoj@gmail.com>
2018-04-09 22:06:13 -0400

    cleanup and back to 1 project file which targets both frameworks

--- src/PerformanceTest.cs
+++ src/PerformanceTest.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PerformanceTester.Core
+namespace PerformanceTester
 {
 
     //CLR = Full Framework
--- src/Program.cs
+++ src/Program.cs
@@ -3,7 +3,7 @@ using System;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PerformanceTester.Core
+namespace PerformanceTester
 {
     class Program
     {
